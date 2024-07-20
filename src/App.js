87c7deafacda3de58baaa7c245a63d82fab99225
Uday Kumar <udaykumar.77348@gmail.com>
2024-07-20 18:01:43 +0530

    Clean up initial files and set up placeholder content

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My YouTube Clone</h1>
+      <p>Stay tuned for updates!</p>
+    </div>
+  );
+}
+
+export default App;
