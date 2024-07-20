4ad42203e6913ae6834b0cc93487868e4a7ea0be
Uday Kumar <udaykumar.77348@gmail.com>
2024-07-20 19:53:35 +0530

    css update

--- src/Pages/home/home.jsx
+++ src/Pages/home/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Home.css'
+import '../home/home.css'
 import Sidebar from '../../components/navbar/sidebar/sidebar'
 
 const Home = () => {
--- src/Pages/video/video.jsx
+++ src/Pages/video/video.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Video.css'
+import '../video/video.css'
 
 const Video = () => {
   return (
